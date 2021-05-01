cdd545c1aa40f38ba1b2fc04e0bc9f9ea1dfcfcd
takushikai <takushikai2@gmail.com>
2021-05-01 11:39:25 +0900

    add new file

--- main.js
+++ main.js
@@ -1,42 +1,170 @@
-var canvas, ctx;
-var x = 0, y = 0, vx = 1, vy = 1;
-//初期処理
-function init(){
-    canvas = document.getElementById("canvas");
-    canvas.width = 640;
-    canvas.height = 900;
-    ctx = canvas.getContext("2d");//コンテキスト
+//未実装
+//マウスイベント,タッチ
+
+//ユーザーに触られたくないものは全てSetting内へ
+const Setting = {
+    canvas:undefined,
+    ctx:undefined,
+    GameObjects:undefined,
+    forEachFrame:undefined,
+    init:function(){
+        Setting.canvas = document.getElementById("canvas");
+        Setting.ctx = Setting.canvas.getContext("2d");
+        Setting.canvas.width = 640;//デフォルト値
+        Setting.canvas.height = 900;
+        Setting.GameObjects = [];
+        Setting.forEachFrame = [];
+        Setting.update();
+    },
+    update:function(){
+        requestAnimationFrame(Setting.update);//フレームごとに
+
+        Setting.ctx.clearRect(0, 0, Setting.canvas.width, Setting.canvas.height);//画面をクリア(前の画面描画を削除)
+        for(let i=0; i<Setting.GameObjects.length; i++){
+            Setting.GameObjects[i].draw();
+        }
+        for(let i=0; i<Setting.forEachFrame.length; i++){
+            Setting.forEachFrame[i]();
+        }
+    },
+
+}
+
+
+class GameObject{//public?
+    constructor(w, h, x, y){
+        this.width = w;
+        this.height = h;
+        this.x = x;
+        this.y = y;
+        this.rotate = 0;//未確認
+        this.name = "";
+        this.tag = "";
+        this.indicate = true;//表示
+    }
+
+    draw(){
+        if(this.indicate){
+            Setting.ctx.fillStyle = "white";
+            Setting.ctx.rotate(this.rotate * Math.PI / 180);//未確認
+            Setting.ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    }
+
+    onClick(callbackfunc){
+        Setting.canvas.addEventListener("click",(event)=>{
+            Setting.forEachFrame.push(()=>{
+                if
+                (
+                    this.indicate &&//表示されていて
+                    //対象の範囲内なら
+                    event.offsetX >= this.x &&
+                    event.offsetX <= this.x+this.width &&
+                    event.offsetY >= this.y &&
+                    event.offsetY <= this.y+this.height
+                ){
+                    callbackfunc();
+                }
+            });
+        });
+    }
+
+    onCollisionEnter(target,callbackfunc){
+        let collisionStarted = false;
+        Setting.forEachFrame.push(()=>{//各フレームごとに実行
+            if(this.indicate){//表示されていて
+                if(target!=undefined && collision(this,target)){
+                    if(!collisionStarted){
+                        callbackfunc();
+                        collisionStarted = true;
+                    }
+                }
+                else if(!collision(this,target)&&collisionStarted){//次の衝突に備える
+                    collisionStarted = false;
+                }
+            }
+        });
+    }
+
+    onCollisionStay(target,callbackfunc){
+        Setting.forEachFrame.push(()=>{
+        if(this.indicate && target!=undefined && collision(this,target)){
+            callbackfunc();
+        }
+        });
+    }
+
+    onCollisionExit(target,callbackfunc){
+        let collisionStarted = false;
+        Setting.forEachFrame.push(()=>{
+            if(this.indicate){//表示されていて
+                if(target!=undefined && collision(this,target)){
+                    collisionStarted = true;
+                }
+                else if(!collision(this,target)&&collisionStarted){
+                    callbackfunc();
+                    collisionStarted = false;
+                }
+            }
+        });
+    }
+}
+
+
+function create(w,h,x,y){//public
+    const obj = new GameObject(w,h,x,y);
+    Setting.GameObjects.push(obj);
+    return obj;
+}
+
+
+function getByName(name){//public
+    for(let i=0; i<Setting.GameObjects.length; i++){
+        if(Setting.GameObjects[i].name==name){
+            return Setting.GameObjects[i];
+        }
+    }
+}
+
+function getByTag(tag){//public
+    const obj = [];
+    for(let i=0; i<Setting.GameObjects.length; i++){
+        if(Setting.GameObjects[i].tag==tag){
+            obj.push(Setting.GameObjects[i]);
+        }
+    }
+
+    return obj;
+}
+
+
+//矩形の当たり判定
+function collision(gameObject1,gameObject2){//public?
+    const centerX_1 = gameObject1.x + gameObject1.width/2;
+    const centerY_1 = gameObject1.y + gameObject1.height/2;
+    const centerX_2 = gameObject2.x + gameObject2.width/2;
+    const centerY_2 = gameObject2.y + gameObject2.height/2;
+
+    if(
+        Math.abs(centerX_1 - centerX_2) <= gameObject1.width/2 + gameObject2.width/2 //横の判定
+        &&
+        Math.abs(centerY_1 - centerY_2) <= gameObject1.height/2 + gameObject2.height/2 //縦の判定
+    )
+    {
+        return true; // hit
+    }
+    else
+    {
+        return false;
+    }
+}
+
+window.addEventListener("load",()=>{
+    Setting.init();
     
-    requestAnimationFrame(update);//フレーム処理のお知らせを送る
-}
-//四角を描く
-function drawRect(x, y, w, h){
-    ctx.fillStyle = "white";
-    ctx.fillRect(x, y, w, h);
-}
-//画面描画処理
-function render(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);//画面をクリア(前の画面描画を削除)
-    x += vx;
-    y += vy;
-    if(x >= canvas.width - 64){
-        vx = -1;
-    }else if(x <= 0){
-        vx = 1;
-    }
-    if(y >= canvas.height - 64){
-        vy = -1;
-    }else if(y <= 0){
-        vy = 1;
-    }
-    drawRect(x, y, 64, 64); 
-}
-//更新処理
-function update(){
-    render();
-    requestAnimationFrame(update);//フレーム処理のお知らせを送る
-}
-//開始
-window.onload = function(){
-    init();
-}
+    //ユーザースクリプトを読み込み
+    const elm = document.createElement("script");
+    elm.src = "./userScript.js";
+    document.body.appendChild(elm);
+
+});
---
+++ userScript.js
@@ -0,0 +1,81 @@
+/**
+ * @version 0.1
+ * @name 決めてない。
+ * 
+ */
+/*
+【設定】
+・画面幅(デフォルト：640)
+Setting.canvas.width = 値;
+・画面高さ(デフォルト：900)
+Setting.canvas.height = 値;
+
+【ゲームオブジェクトを作る】
+create(w,h,x,y);
+w:幅(number)
+h:高さ(number)
+x,y:座標(number)
+
+【ゲームオブジェクトを取得する】
+getByName(name);
+name:ゲームオブジェクトの名前(string)
+
+getByTag(tag);
+tag:タグ(string)
+※戻り値は配列であることに注意
+
+
+※以下、「GameObject」はゲームオブジェクトが格納された変数を指す。
+
+【ゲームオブジェクトの操作】
+以下は、「=」で値を代入することで変更出来る。
+GameObject.width
+GameObject.height
+GameObject.x
+GameObject.y
+GameObject.rotate
+GameObject.name
+GameObject.tag
+
+Object.indicate = false;
+とすることで、GameObjectを非表示に出来る。
+(再表示するときは Object.indicate = true;)
+
+
+【ゲームオブジェクト組み込み関数(厳密にはメソッド)】
+・クリックされたとき
+GameObject.onClick(callbackFunction);
+callbackFunction:関数
+
+・衝突開始時
+GameObject.onCollisionEnter(target, callbackFunction);
+target:対象のGameObject
+callbackFunction:関数(衝突時に一度だけ呼び出される)
+
+・衝突中
+GameObject.onCollisionStay(target, callbackFunction);
+target:対象のGameObject
+callbackFunction:関数(衝突している間、各フレームごとに呼び出される)
+
+・衝突後
+GameObject.onCollisionExit(target, callbackFunction);
+target:対象のGameObject
+callbackFunction:関数(衝突後に一度だけ呼び出される)
+
+*/
+
+
+//以下サンプルスクリプト
+
+const obj1 = create(10,10,10,10);
+const obj2 = create(10,10,50,10);
+
+//ぶつかったらobj1を消す
+obj1.onCollisionEnter(obj2,()=>{
+    // obj1.indicate = false;
+});
+
+//obj1を１秒ごとにxに方向に10だけ移動させる
+setInterval(()=>{
+    obj1.x+=10;
+}, 1000);
